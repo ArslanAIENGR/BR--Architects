b1ad2d0b00f80919e8576b6b5ee9fc8aec523e70
Arslan Ahmed <168894861+ArslanAIENGR@users.noreply.github.com>
2024-07-18 21:54:08 +0500

    Delete assets directory

--- assets/js/script.js
+++
@@ -1,8 +0,0 @@
-document.addEventListener("DOMContentLoaded", function() {
-    const hamburger = document.getElementById('hamburger');
-    const navRight = document.getElementById('navRight');
-
-    hamburger.addEventListener('click', function() {
-        navRight.classList.toggle('show');
-    });
-});
